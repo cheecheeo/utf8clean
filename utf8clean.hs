e699aa2cb8dead62413b5317c755f89e8278767f
John Chee <cheecheeo@gmail.com>
2012-09-14 15:15:31 -0700

    Initial work on verify mode

--- utf8clean.hs
+++ utf8clean.hs
@@ -4,12 +4,18 @@
 -- TODO verify mode, cleanup mode
 -- TODO in verify mode, report _all_ failures (line + byte in line + total byte offset)
 
+import           Control.Applicative        ((<$>))
+import           Data.Char                  (GeneralCategory(..))
+import           Data.Char                  as C
+import           Data.Int                   (Int64)
+
+import           Data.ByteString.Lazy.Char8 (ByteString)
 import qualified Data.ByteString.Lazy.Char8 as L8
-import Data.Text.Lazy (Text)
-import qualified Data.Text.Lazy.IO as TIO
-import qualified Data.Text.Lazy.Encoding as DTE
-import Data.Text.Encoding.Error (OnDecodeError)
-import qualified Data.Text.Encoding.Error as DTEE
+import           Data.Text.Encoding.Error   (OnDecodeError)
+import qualified Data.Text.Encoding.Error   as DTEE
+import           Data.Text.Lazy             (Text)
+import qualified Data.Text.Lazy.Encoding    as DTE
+import qualified Data.Text.Lazy.IO          as TIO
 
 -- Copied from: http://hackage.haskell.org/packages/archive/text/0.11.2.3/doc/html/src/Data-Text-Lazy-IO.html#hGetContents
 getContentsDecodedWith :: OnDecodeError -> IO Text
@@ -18,6 +24,24 @@ getContentsDecodedWith ode = fmap (DTE.decodeUtf8With ode) L8.getContents
 interactDecodeWith :: OnDecodeError -> (Text -> Text) -> IO ()
 interactDecodeWith ode f = TIO.putStr . f =<< (getContentsDecodedWith ode)
 
+-- TODO format characters are probably valid
+isWeird :: Char -> Bool
+isWeird c =
+  case C.generalCategory c of
+    Format      -> True
+    NotAssigned -> True
+    PrivateUse  -> True
+    Surrogate   -> True
+    _           -> False
+
+weirdCharactersWithIndices :: ByteString -> [(Int64, Char)]
+weirdCharactersWithIndices bs = snd $ L8.foldr (\c (oldIndex, ics) ->
+  let currentIndex = oldIndex + 1
+  in (currentIndex, if isWeird c then (currentIndex, c) : ics else ics))
+  (0, []) bs
+
 main :: IO ()
-main = interactDecodeWith DTEE.lenientDecode id
---main = interactDecodeWith DTEE.strictDecode id
+--main = interactDecodeWith DTEE.lenientDecode id
+main = print =<< (weirdCharactersWithIndices <$> L8.getContents)
+--main = print =<< (L8.findIndices isWeird <$> L8.getContents)
+--main = putStr =<< (unlines . map (show . C.generalCategory) . L8.unpack <$> L8.getContents)
